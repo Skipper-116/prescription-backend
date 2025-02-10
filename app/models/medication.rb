b58ec6966d0c6eacca02d035d76c93417ecb3e2e
Roy Chanunkha <roychanunkha@gmail.com>
2025-02-10 11:04:20 -0800

    chore: medication

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
+# Medication model
 class Medication < ApplicationRecord
+  has_many :dosages
+
+  validates :name, presence: true
+  validates :unit_price, presence: true
+
+  def as_json(options = {})
+    super(options.merge(only: [ :id, :name, :unit_price ]))
+  end
 end
